238e1c1ad6e8f9ff920e669bf92917e98a38b5fa
2400030514 <nunnahasini7@gmail.com>
2026-04-27 21:00:49 +0530

    Initial commit

--- src/pages/api.js
+++ src/pages/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "https://skillvault-backend1-production.up.railway.app/api",
 });
 
 api.interceptors.request.use((config) => {
