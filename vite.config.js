0bc3d42c20f63a1fb35b59cc99b648012a517aa2
NUNNA HASINI <nunnahasini7@gmail.com>
2026-04-27 21:28:38 +0530

    Add preview configuration with allowed hosts

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/"
+  base: "/",
+  preview: {
+    allowedHosts: ['skillvault-frontend1-production.up.railway.app']
+  }
 });
